091c76c9e4f2c3d536f32851a832df60108ba3d7
Erick Medeiros <ericksmedeiros@gmail.com>
2016-12-03 22:12:42 -0200

    Criação Menu Simples

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,40 @@
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Olá Mundo");
+
+		menu();
+	}
+
+	private static void menu() {
+		System.out.println("1 - Inserir no Inicio");
+		System.out.println("2 - Inserir no Final");
+		System.out.println("3 - Pesquisar Valor");
+		System.out.println("4 - Listar Lista ");
+		switch (digita("Escolha uma opção: ")) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;			
+		default:
+			break;
+		}
+		
+	}
+
+	private static int digita(String mens) {
+		Scanner e = new Scanner(System.in);		
+		System.out.println(mens);
+		return e.nextInt();
 	}
 
 }
